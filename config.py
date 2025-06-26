4d105059d397999ec735507151eeb960a5fccb15
LOXXD <sijithappu7902@gmail.com>
2025-06-26 22:59:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 API_ID = 22205059
 API_HASH = "1d92d05c27a74a09ecb75ae2f236b939"
 BOT_TOKEN = "8097353898:AAEiq-v-mcrqvwfIWyb0yWVkUxwOb_P8RnE"
+OWNER_ID = 7748357517  # replace with your Telegram user ID
 
 MONGO_URI = "mongodb+srv://renamebot:renamebot@cluster0.duek2xr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DUMP_CHANNEL = -1002569538083
